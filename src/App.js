c72a65cb5b6e166a005a62a33677bf466687fc1e
Joyesh Debnath <itsmeJoyeshdebnath26@gmail.com>
2021-08-12 21:55:34 +0530

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Cart from "./components/Cart/Cart";
+
 function App() {
 	return (
 		<BrowserRouter>
 			<Header />
+			<div>
+				<Route path="/" exact>
+					<Home />
+				</Route>
+				<Route path="/cart" exact>
+					<Cart />
+				</Route>
+			</div>
 		</BrowserRouter>
 	);
 }
